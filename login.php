cca3ef6a8a96fa01ba7beb0eba935dd556d05ffd
Trafel Selfi <selfitrafel@gmail.com>
2023-10-19 13:08:46 +0700

    edit body

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Toko</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body id="bg-login">
+    <div class="box-login">
+        <h2>Login</h2>
+        <form action="" method="POST">
+            <input type="text" name="user" placeholder="Username" required>
+            <input type="password" name="pass" placeholder="Password" required>
+            <input type="submit" name="submit" placeholder="Login" required>
+        </form>
+    </div>
+</body>
+</html>
