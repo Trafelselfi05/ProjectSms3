245b077f07d33375e023bce3ac78411996101fa1
Trafel Selfi <selfitrafel@gmail.com>
2023-10-19 19:59:03 +0700

    edit

--- login.php
+++ login.php
@@ -5,15 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Toko</title>
     <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
 </head>
 <body id="bg-login">
     <div class="box-login">
         <h2>Login</h2>
         <form action="" method="POST">
-            <input type="text" name="user" placeholder="Username" required>
-            <input type="password" name="pass" placeholder="Password" required>
-            <input type="submit" name="submit" placeholder="Login" required>
+            <input type="text" name="user" placeholder="Username" required class="input-control">
+            <input type="password" name="pass" placeholder="Password" required class="input-control">
+            <input type="submit" name="submit" value="Login" required class="btn">
         </form>
+        <?php
+        if(isset($_POST["submit"])) {
+            include 'db.php';
+
+            $user = $_POST["user"];
+            $pass = $_POST["pass"];
+
+            $cek = mysqli_query($conn, "SELECT * FROM  tb_admin WHERE username = '".$user."' AND password = '".MD5($pass)."'");
+            if (mysqli_num_rows($cek) > 0    ) {
+                echo '<script>window.location="dashboard.php"</script>';
+            }else {
+                echo '<script>alert("Username atau Password Anda salah!")</script>';
+            }
+        }
+        ?>
     </div>
 </body>
 </html>
