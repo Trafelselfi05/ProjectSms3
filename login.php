ab8adcd08996800182e59a90a04fb94238b7ddca
Trafel Selfi <selfitrafel@gmail.com>
2023-10-25 16:42:02 +0700

    edit body

--- login.php
+++ login.php
@@ -16,14 +16,21 @@
             <input type="submit" name="submit" value="Login" required class="btn">
         </form>
         <?php
+        
         if(isset($_POST["submit"])) {
+            session_start();
             include 'db.php';
 
             $user = $_POST["user"];
             $pass = $_POST["pass"];
 
             $cek = mysqli_query($conn, "SELECT * FROM  tb_admin WHERE username = '".$user."' AND password = '".MD5($pass)."'");
-            if (mysqli_num_rows($cek) > 0    ) {
+            if (mysqli_num_rows($cek) > 0 ) {
+                $d = mysqli_fetch_object($cek);
+                $_SESSION['status_login'] = true;
+                $_SESSION['a_global'] =$d;
+               
+
                 echo '<script>window.location="dashboard.php"</script>';
             }else {
                 echo '<script>alert("Username atau Password Anda salah!")</script>';
