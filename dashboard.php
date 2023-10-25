2232f0fe63fd9f4c185cb0e859dede5240f629a4
Trafel Selfi <selfitrafel@gmail.com>
2023-10-25 19:50:50 +0700

    footer

--- dashboard.php
+++ dashboard.php
@@ -13,12 +13,29 @@
     <div class="container">
     <h1><a href="dashboard.php">TokoKarmaIndah</a></h1>
     <ul>
-        <li><a href="">Dashboard</a></li>
-        <li><a href="">Profil</a></li>
-        <li><a href="">Data Kategori</a></li>
-        <li><a href="">Data Produk</a></li>
+        <li><a href="dashboard.php">Dashboard</a></li>
+        <li><a href="profil.php">Profil</a></li>
+        <li><a href="data-kategori.php">Data Kategori</a></li>
+        <li><a href="data-produk.php">Data Produk</a></li>
+        <li><a href="keluar.php">Keluar</a></li>
     </ul>
     </div>
    </header>
+   <!--Content-->
+   <div class="section">
+    <div class="container">
+        <h3>Dashboard</h3>
+        <div class="box">
+            <h4>Selamat Datang</h4>
+        </div>
+    </div>
+   </div>
+
+   <!--Footer-->
+   <footer>
+    <div class="container">
+        <small>Copyright &copy; 2023 -TOKOKARMAINDAH.</small>
+    </div>
+   </footer>
 </body>
 </html>
