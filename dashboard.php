04f9c0867aa0a84506f95ab5d950e4301b7e8b34
Trafel Selfi <selfitrafel@gmail.com>
2023-10-25 18:04:31 +0700

    edit body

---
+++ dashboard.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toko</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+<body>
+   <!--Header-->
+   <header>
+    <div class="container">
+    <h1><a href="dashboard.php">TokoKarmaIndah</a></h1>
+    <ul>
+        <li><a href="">Dashboard</a></li>
+        <li><a href="">Profil</a></li>
+        <li><a href="">Data Kategori</a></li>
+        <li><a href="">Data Produk</a></li>
+    </ul>
+    </div>
+   </header>
+</body>
+</html>
